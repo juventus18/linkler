9f10183db7816489efa6a4e90ee5672de7687217
Sean R <reevessp@unr.edu>
2020-12-16 19:25:51 -0800

    7

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on('message', async message => {
 	// This event will run on every single message received, from any channel or DM.
 
 	// It's good practice to ignore other bots. This also makes your bot ignore itself
-	// and not get into a spam loop (we call that "botception").
+	// and not get into a spam loop (we call that "botception")
 	if (message.author.bot) return;
 
 	if (message.content.includes("http")) {
